8568d3ddfe8531a8e882bfaee9d375c6669c193e
Mystic <mystic091717@gmail.com>
2023-10-13 16:02:24 +0900

    20231013 basic features completed

--- src/components/movies/MovieDetail.js
+++ src/components/movies/MovieDetail.js
@@ -8,12 +8,20 @@ import { reviewActions } from "../../toolkit/actions/review_action";
 import styled from "styled-components";
 import { PiThumbsUp, PiThumbsUpFill } from 'react-icons/pi';
 import { RxChatBubble } from 'react-icons/rx'
+import { TbEyePlus } from 'react-icons/tb'
+import { IoMdEye, IoMdEyeOff } from 'react-icons/io'
+import axios from "axios";
 
 const MovieDetail = () => {
   const { movieId } = useParams();
   console.log("MovieDetail.js movieId: ", movieId);
 
   const dispatch = useDispatch();
+
+  const [eyeState, setEyeState] = useState(false);
+
+  //좋아요 추가,삭제 버튼 쿨다운
+  const [isClickOnCool, setIsClickOnCool] = useState();
 
   //모달창 상태값 변수
   const [isModalOpen, setIsModalOpen] = useState(false);
@@ -25,11 +33,55 @@ const MovieDetail = () => {
   const closeModal = () => setIsModalOpen(false);
 
   const getMovieDetail = (movieId) => {
-    dispatch(movieActions.getMovieDetail(movieId));
+    dispatch(movieActions.getMovieDetail(movieId, localStorage.getItem('clientId')));
   };
 
   const getReviewList = (movieId) => {
     dispatch(reviewActions.getReviewList(movieId, localStorage.getItem('clientId')));
+  }
+
+  const config = {
+    headers: {
+      Authorization: localStorage.getItem('authorization'),
+    },
+  };
+
+  const addViewCount = async () => {
+    if (isClickOnCool) {
+      return;
+    }
+
+    setIsClickOnCool(true);
+
+    setTimeout(() => {
+      setIsClickOnCool(false);
+    }, 1000);
+
+    const formData = new FormData();
+    formData.append('movieId', movieId);
+    formData.append('clientId', localStorage.getItem('clientId'));
+
+    await axios
+      .post(`/view/add`, formData, config);
+
+    getMovieDetail(movieId);
+  }
+
+  const removeViewCount = async () => {
+    if (isClickOnCool) {
+      return;
+    }
+
+    setIsClickOnCool(true);
+
+    setTimeout(() => {
+      setIsClickOnCool(false);
+    }, 1000);
+
+    await axios
+      .delete(`/view/delete/${movieId}/${localStorage.getItem('clientId')}`, config);
+
+    getMovieDetail(movieId);
   }
 
   useEffect(() => {
@@ -51,7 +103,38 @@ const MovieDetail = () => {
             <img className="poster-box" src={movie.poster} />
           </div>
           <div className="col-9" style={{ padding: 40 }}>
-            <h1 style={{ fontWeight: 800 }}>{movie.title}</h1>
+            <div className="d-flex">
+              <div className="me-auto">
+                <h1 style={{ fontWeight: 800 }}>{movie.title}</h1>
+              </div>
+              <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+                <Eyes>
+                  {movie.viewCheck ? (
+                    <div
+                      onMouseEnter={() => setEyeState(true)} // 마우스 호버 시 `eyeState`를 true로 변경
+                      onMouseLeave={() => setEyeState(false)} // 마우스가 벗어날 때 `eyeState`를 false로 변경
+                    >
+                      {eyeState ? (
+                        <span onClick={removeViewCount}>
+                          <IoMdEyeOff className="opened-eye" size='50px' />
+                        </span>
+                      ) : (
+                        <span onClick={removeViewCount}>
+                          <IoMdEye className="closed-eye" size='50px' />
+                        </span>
+                      )}
+                    </div>
+                  ) : (
+                    <span onClick={addViewCount}>
+                      <TbEyePlus className="closed-eye" size='50px' />
+                    </span>
+                  )}
+                </Eyes>
+                <span className="mt-1" style={{ fontSize: '12px' }}>본 적 있어요</span>
+              </div>
+            </div>
+            <hr />
+            <p>우리 회원 중 {movie.viewCount} 명이 이 작품을 봤어요.</p>
             <hr />
             <p>{movie.movieDescription}</p>
           </div>
@@ -166,5 +249,29 @@ const Stars = styled.div`
 
   svg.yellowStar {
     color: red;
+  }
+`;
+
+const Eyes = styled.div`
+  svg {
+    transition: all 100ms
+  }
+
+  svg .opened-eye{
+    color:black
+  }
+
+  svg .closed-eye{
+    color:grey
+  }
+
+  svg:hover {
+    color:red;
+    scale:1.2;
+  }
+  
+  svg:active {
+    color:red;
+    scale:1.5;
   }
 `;
